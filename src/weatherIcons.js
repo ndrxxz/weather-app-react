31ce496754a4a56095a2ae431a262d341ee64b9d
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-04-28 18:03:49 +0800

    add weather icons

---
+++ src/weatherIcons.js
@@ -0,0 +1,17 @@
+import atmosphere from "./assets/svg/atmosphere.svg";
+import clear from "./assets/svg/clear.svg";
+import clouds from "./assets/svg/clouds.svg";
+import drizzle from "./assets/svg/drizzle.svg";
+import rain from "./assets/svg/rain.svg";
+import snow from "./assets/svg/snow.svg";
+import thunderstorm from "./assets/svg/thunderstorm.svg";
+
+export const weatherIcons = {
+    atmosphere: atmosphere,
+    clear: clear,
+    clouds: clouds,
+    drizzle: drizzle,
+    rain: rain,
+    snow: snow,
+    thunderstorm: thunderstorm,
+};
