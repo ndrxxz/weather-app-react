bf48dab9cd9eb70df271d8a049a99007455fe460
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-05 18:35:40 +0800

    update svg path

--- src/utils/weatherIcons.js
+++ src/utils/weatherIcons.js
@@ -1,10 +1,10 @@
-import atmosphere from "./assets/svg/atmosphere.svg";
-import clear from "./assets/svg/clear.svg";
-import clouds from "./assets/svg/clouds.svg";
-import drizzle from "./assets/svg/drizzle.svg";
-import rain from "./assets/svg/rain.svg";
-import snow from "./assets/svg/snow.svg";
-import thunderstorm from "./assets/svg/thunderstorm.svg";
+import atmosphere from "../assets/svg/atmosphere.svg";
+import clear from "../assets/svg/clear.svg";
+import clouds from "../assets/svg/clouds.svg";
+import drizzle from "../assets/svg/drizzle.svg";
+import rain from "../assets/svg/rain.svg";
+import snow from "../assets/svg/snow.svg";
+import thunderstorm from "../assets/svg/thunderstorm.svg";
 
 export const weatherIcons = {
   atmosphere: atmosphere,
