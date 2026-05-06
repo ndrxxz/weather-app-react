06ba7aa379d994d3bb62555ad0414ae1b9282e0d
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-06 18:03:46 +0800

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { RiSunFill } from "react-icons/ri";
-import Search from "./assets/message/search.jsx";
-import NotFound from "./assets/message/not-found.jsx";
 import SearchBar from "./components/SearchBar";
 import WeatherCard from "./components/WeatherCard.jsx";
 import HourlyForecast from "./components/HourlyForecast.jsx";
-import WeeklyForecast from "./components/WeeklyForecast";
+import WeeklyForecast from "./components/WeeklyForecast.jsx";
 import UseWeather from "./hooks/UseWeather.js";
+import EmptyState from "./components/EmptyState.jsx";
+import searchNotFound from "./assets/svg/not-found.svg";
+import searchImg from "./assets/svg/search.svg";
 
 function App() {
   const [city, setCity] = useState("");
-  const { weather, forecast } = UseWeather(city);
-
-  const handleSearch = (searchedCity) => {
-    setCity(searchedCity);
-  };
+  const { weather, forecast, notFound } = UseWeather(city);
 
   return (
     <div className="min-h-screen bg-black text-white">
@@ -28,22 +25,19 @@ function App() {
           <RiSunFill />
         </button>
       </nav>
+      
 
       {/* Hero */}
       <div className="flex flex-col items-center gap-6 sm:gap-8 px-4 text-center">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
           How's the sky looking today?
         </h1>
-        <SearchBar onSearch={handleSearch} />
+        <SearchBar onSearch={setCity} />
       </div>
 
-      {!weather && !forecast && (
-        <Search />
-      )}
-
-      {/* <NotFound /> */}
-
-      {weather && forecast && (
+      {notFound ? (
+        <EmptyState img={searchNotFound} desc="City not found." />
+      ) : weather && forecast ? (
         <div className="px-4 mt-8 flex justify-center">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-5xl pb-10">
             <div className="flex flex-col gap-4 md:col-span-2">
@@ -53,6 +47,11 @@ function App() {
             <WeeklyForecast forecast={forecast} />
           </div>
         </div>
+      ) : (
+        <EmptyState
+          img={searchImg}
+          desc="Search for a city to get the current weather and forecast."
+        />
       )}
     </div>
   );
