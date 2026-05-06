0a82e388996c97686af4d150287e02d13ee244a2
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-06 18:05:07 +0800

    update hook

--- src/hooks/UseWeather.js
+++ src/hooks/UseWeather.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function UseWeather(city) {
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [notFound, setNotFound] = useState(false);
 
   const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
@@ -11,30 +12,36 @@ function UseWeather(city) {
 
     const fetchWeather = async () => {
       try {
+        setNotFound(false);
+
         const [weatherRes, forecastRes] = await Promise.all([
-          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`),
-          fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`),
+          fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+          ),
+          fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`,
+          ),
         ]);
 
-        if (!weatherRes.ok || !forecastRes.ok) throw new Error("city not found");
+        if (!weatherRes.ok || !forecastRes.ok)
+          throw new Error("city not found");
 
         const weatherData = await weatherRes.json();
         const forecastData = await forecastRes.json();
 
         setWeather(weatherData);
         setForecast(forecastData);
-
-        console.log(weatherData);
-        console.log(forecastData)
       } catch (err) {
-        console.error(err.message);
+        setWeather(null);
+        setForecast(null);
+        setNotFound(true);
       }
-    }
+    };
 
     fetchWeather();
   }, [city]);
 
-  return { weather, forecast };
+  return { weather, forecast, notFound };
 }
 
-export default UseWeather
+export default UseWeather;
