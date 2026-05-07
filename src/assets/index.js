7381d0876a9ae645482f7917b6c9ef2b4dd4cf42
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-07 17:49:31 +0800

    create a barrel file

---
+++ src/assets/index.js
@@ -0,0 +1,2 @@
+export { default as searchNotFound } from "./svg/not-found.svg";
+export { default as searchImg } from "./svg/search.svg";
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as EmptyState } from "./common/EmptyState";
+export { default as Navbar } from "./layout/Navbar";
+export { default as Hero } from "./weather/Hero";
+export { default as HourlyForecast } from "./weather/HourlyForecast";
+export { default as SearchBar } from "./weather/SearchBar";
+export { default as WeatherCard } from "./weather/WeatherCard";
+export { default as WeatherContent } from "./weather/WeatherContent";
+export { default as WeeklyForecast } from "./weather/WeeklyForecast";
