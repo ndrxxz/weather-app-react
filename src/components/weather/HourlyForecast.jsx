0f78e8cd7236a897c74f9545f0d2dac48e6f766e
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-07 17:50:34 +0800

    update components content

---
+++ src/components/weather/HourlyForecast.jsx
@@ -0,0 +1,46 @@
+import { weatherIcons, getWeatherType } from "@/utils";
+
+export default function HourlyForecast({ forecast }) {
+  if (!forecast) return null;
+
+  const hourlyData = forecast.list.slice(0, 8);
+
+  return (
+    <div className="bg-zinc-900 backdrop-blur-md border border-white/10 rounded-lg p-4 space-y-4">
+      <h2 className="text-base sm:text-lg md:text-xl font-semibold">
+        Hourly Forecast
+      </h2>
+
+      <div className="overflow-x-auto no-scrollbar scroll-smooth snap-x snap-mandatory scroll-px-4 -mx-4">
+        <div className="flex gap-2 px-4">
+          {hourlyData.map((hour) => {
+            const time = new Date(hour.dt * 1000).toLocaleTimeString("en-US", {
+              hour: "numeric",
+              hour12: true,
+            });
+
+            const temp = hour.main.temp;
+            const weatherType = getWeatherType(hour.weather[0].id);
+            const weatherIcon = weatherIcons[weatherType];
+
+            return (
+              <div
+                key={hour.dt}
+                className="flex flex-col items-center min-w-24 snap-start rounded-lg p-3 bg-white/5 border border-white/10 transition-all duration-300 ease-out will-change-transform hover:bg-white/10"
+              >
+                <span className="text-xs text-gray-400">{time}</span>
+                <img src={weatherIcon} alt={weatherType} />
+                <span className="text-sm font-semibold">
+                  {`${Math.round(temp)}`}
+                  <span className="text-xs align-top">°C</span>
+                </span>
+              </div>
+            );
+          })}
+
+          <div className="min-w-2 shrink-0" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/weather/WeeklyForecast.jsx
@@ -0,0 +1,50 @@
+import { weatherIcons, getWeatherType } from "@/utils";
+
+export default function Weeklyforecast({ forecast }) {
+  if (!forecast) return null;
+
+  const weeklyData = forecast.list.filter((item) =>
+    item.dt_txt.includes("12:00:00"),
+  );
+
+  return (
+    <div className="bg-zinc-900 backdrop-blur-md border border-white/10 p-4 rounded-lg space-y-4">
+      <h2 className="text-base sm:text-lg md:text-xl font-semibold">
+        5-Day Forecast
+      </h2>
+
+      {weeklyData.map((week) => {
+        const date = new Date(week.dt * 1000).toLocaleDateString("en-US", {
+          weekday: "short",
+        });
+
+        const description = week.weather[0].description;
+        const temp = week.main.temp;
+        const temp_min = week.main.temp_min;
+        const weatherType = getWeatherType(week.weather[0].id);
+        const weatherIcon = weatherIcons[weatherType];
+
+        return (
+          <div
+            key={week.dt}
+            className="flex items-center justify-between rounded-lg px-3 py-4.5 bg-white/5 border border-white/10 hover:bg-white/10 transition-all duration-300 ease-out will-change-transform hover:-translate-y-1 hover:scale-[1.02]"
+          >
+            <div className="flex flex-col gap-1 text-sm font-semibold">
+              <span className="text-md w-10">{date}</span>
+              <span className="text-xs text-gray-400 capitalize">
+                {description}
+              </span>
+            </div>
+            <img src={weatherIcon} alt={weatherType} className="w-12 h-12" />
+            <div className="flex flex-col gap-2 text-md font-semibold leading-3">
+              <span>{`${Math.round(temp)}`}°C</span>
+              <span className="text-gray-400 text-xs text-right">
+                {`${Math.round(temp_min)}`}°C
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
