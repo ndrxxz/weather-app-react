98c6e2cfa6eb663c77e929112d1c70fd648edb10
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-08 16:17:06 +0800

    add dark and light toggle

--- src/components/weather/HourlyForecast.jsx
+++ src/components/weather/HourlyForecast.jsx
@@ -1,17 +1,27 @@
+import { Scroll } from "@/hooks";
 import { weatherIcons, getWeatherType } from "@/utils";
 
 export default function HourlyForecast({ forecast }) {
+  const drag = Scroll();
+
   if (!forecast) return null;
 
   const hourlyData = forecast.list.slice(0, 8);
 
   return (
-    <div className="bg-zinc-900 backdrop-blur-md border border-white/10 rounded-lg p-4 space-y-4">
+    <div className="bg-zinc-900 light:bg-white backdrop-blur-md border border-white/10 light:border-slate-200 rounded-lg p-4 space-y-4">
       <h2 className="text-base sm:text-lg md:text-xl font-semibold">
         Hourly Forecast
       </h2>
 
-      <div className="overflow-x-auto no-scrollbar scroll-smooth snap-x snap-mandatory scroll-px-4 -mx-4">
+      <div
+        ref={drag.ref}
+        onMouseDown={drag.onMouseDown}
+        onMouseUp={drag.onMouseUp}
+        onMouseLeave={drag.onMouseLeave}
+        onMouseMove={drag.onMouseMove}
+        className="overflow-x-auto no-scrollbar cursor-grab active:cursor-grabbing scroll-smooth snap-x snap-mandatory scroll-px-4 -mx-4"
+      >
         <div className="flex gap-2 px-4">
           {hourlyData.map((hour) => {
             const time = new Date(hour.dt * 1000).toLocaleTimeString("en-US", {
@@ -26,10 +36,14 @@ export default function HourlyForecast({ forecast }) {
             return (
               <div
                 key={hour.dt}
-                className="flex flex-col items-center min-w-24 snap-start rounded-lg p-3 bg-white/5 border border-white/10 transition-all duration-300 ease-out will-change-transform hover:bg-white/10"
+                className="flex flex-col items-center gap-1 min-w-24 snap-start rounded-lg p-3 bg-white/5 light:bg-slate-50 border border-white/10 light:border-slate-200 transition-all duration-300 ease-out will-change-transform hover:bg-white/10 light:hover:bg-slate-100"
               >
-                <span className="text-xs text-gray-400">{time}</span>
-                <img src={weatherIcon} alt={weatherType} />
+                <span className="text-xs text-zinc-400 light:text-zinc-600">
+                  {time}
+                </span>
+                <div className="light:bg-sky-100 rounded-full p-2 flex items-center justify-center">
+                  <img src={weatherIcon} alt={weatherType} />
+                </div>
                 <span className="text-sm font-semibold">
                   {`${Math.round(temp)}`}
                   <span className="text-xs align-top">°C</span>
