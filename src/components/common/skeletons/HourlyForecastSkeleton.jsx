4e5c4054cc668631aed665927ad42f5aa61f917e
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-11 18:18:05 +0800

    update

--- src/components/common/skeletons/HourlyForecastSkeleton.jsx
+++ src/components/common/skeletons/HourlyForecastSkeleton.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function HourlyForecastSkeleton() {
-  return (
-    <div></div>
-  )
+  return <div></div>;
 }
 
-export default HourlyForecastSkeleton
+export default HourlyForecastSkeleton;
