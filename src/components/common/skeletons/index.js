50e8df95d51a1b56ba575f96dbb525e924794612
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-11 18:06:55 +0800

    refactor export files

---
+++ src/components/common/skeletons/index.js
@@ -0,0 +1,4 @@
+export { default as HourlyForecastSkeleton } from "./HourlyForecastSkeleton";
+export { default as WeatherCardSkeleton } from "./WeatherCardSkeleton";
+export { default as WeeklyForecastSkeleton } from "./WeeklyForecastSkeleton";
+export { default as Skeleton } from "./Skeleton";
