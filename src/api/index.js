997305601209236533051df9f4e5f4f617fe1c1b
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-11 18:06:01 +0800

    refactor api fetching using react query

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+export { fetchWeather, fetchForecast } from "./fetchWeather";
--- src/hooks/UseWeather.js
+++ src/hooks/UseWeather.js
@@ -1,47 +1,28 @@
-import React, { useEffect, useState } from "react";
+import { fetchForecast, fetchWeather } from "@/api";
+import { useQueries } from "@tanstack/react-query";
 
 function UseWeather(city) {
-  const [weather, setWeather] = useState(null);
-  const [forecast, setForecast] = useState(null);
-  const [notFound, setNotFound] = useState(false);
+  const [weatherQuery, forecastQuery] = useQueries({
+    queries: [
+      {
+        queryKey: ["weather", city],
+        queryFn: () => fetchWeather(city),
+        enabled: !!city,
+      },
+      {
+        queryKey: ["forecast", city],
+        queryFn: () => fetchForecast(city),
+        enabled: !!city,
+      },
+    ],
+  });
 
-  const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-
-  useEffect(() => {
-    if (!city) return;
-
-    const fetchWeather = async () => {
-      try {
-        setNotFound(false);
-
-        const [weatherRes, forecastRes] = await Promise.all([
-          fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
-          ),
-          fetch(
-            `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`,
-          ),
-        ]);
-
-        if (!weatherRes.ok || !forecastRes.ok)
-          throw new Error("city not found");
-
-        const weatherData = await weatherRes.json();
-        const forecastData = await forecastRes.json();
-
-        setWeather(weatherData);
-        setForecast(forecastData);
-      } catch (err) {
-        setWeather(null);
-        setForecast(null);
-        setNotFound(true);
-      }
-    };
-
-    fetchWeather();
-  }, [city]);
-
-  return { weather, forecast, notFound };
+  return {
+    weather: weatherQuery.data,
+    forecast: forecastQuery.data,
+    isLoading: weatherQuery.isLoading || forecastQuery.isLoading,
+    isError: weatherQuery.isError || forecastQuery.isError,
+  };
 }
 
 export default UseWeather;
