1597cdcf0d5c26a0308fdeaa4c8158471c3db4e2
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-14 17:52:48 +0800

    add style in the btn

--- src/components/common/ErrorBoundary.tsx
+++ src/components/common/ErrorBoundary.tsx
@@ -19,17 +19,20 @@ class ErrorBoundary extends Component<any, ErrorBoundaryState> {
   render() {
     if (this.state.hasError) {
       return (
-        <>
+        <div className="flex flex-col items-center justify-center min-h-screen">
           <EmptyState img={serviceUnavailable} desc="Something went wrong." />
-          <button onClick={() => this.setState({ hasError: false })}>
+          <button
+            className="block mx-auto w-auto bg-zinc-600 hover:bg-zinc-500 text-white rounded-lg px-5 py-2 transition shadow-md cursor-pointer"
+            onClick={() => this.setState({ hasError: false })}
+          >
             Try Again
           </button>
-        </>
+        </div>
       );
     }
 
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export default ErrorBoundary
+export default ErrorBoundary;
