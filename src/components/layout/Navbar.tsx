fe12460b793f702f241270b436f785cb52745311
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-05-14 17:54:01 +0800

    refactor: remove previous code and apply custom hook

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from "react";
+import { useTheme } from "@/context/ThemeContext";
 import { RiMoonFill, RiSunFill } from "react-icons/ri";
 
 function Navbar() {
-  const [hasChanged, setHasChanged] = useState(() => {
-    return localStorage.getItem("theme") === "light";
-  });
-
-  useEffect(() => {
-    if (hasChanged) {
-      document.documentElement.classList.add("light");
-      localStorage.setItem("theme", "light");
-    } else {
-      document.documentElement.classList.remove("light");
-      localStorage.setItem("theme", "dark");
-    }
-  }, [hasChanged]);
-
-  const handleToggle = () => setHasChanged((prev) => !prev);
+  const { isDark, toggleTheme } = useTheme();
 
   return (
     <nav className="light:bg-white border-b border-white/10 light:border-slate-200 flex items-center justify-between px-6 sm:px-6 py-4 mb-6">
@@ -25,10 +11,10 @@ function Navbar() {
       </div>
 
       <button
-        onClick={handleToggle}
+        onClick={toggleTheme}
         className="p-2 rounded-lg hover:bg-zinc-800 light:hover:bg-zinc-300 text-xl sm:text-2xl"
       >
-        {hasChanged ? <RiMoonFill /> : <RiSunFill />}
+        {isDark ? <RiMoonFill /> : <RiSunFill />}
       </button>
     </nav>
   );
